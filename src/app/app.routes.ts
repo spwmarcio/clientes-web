52bc8fbcdef571a0219d03276c38fb656e72fe46
Marcio Fonseca de Souza <spwmarcio@gmail.com>
2026-04-15 00:59:34 +0100

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { CadastrarClientes } from './pages/cadastrar-clientes/cadastrar-clientes';
+import { ConsultarClientes } from './pages/consultar-clientes/consultar-clientes';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: 'cadastrar-clientes', //rota.
+        component: CadastrarClientes //componente.
+    },
+    {
+        path: 'consultar-clientes',  //rota.
+        component: ConsultarClientes //componente.
+    },
+    {
+        path: 'dashboard', //rota.
+        component: Dashboard //componente.
+    },
+    {
+        path: '', pathMatch: 'full', //raiz do projeto
+        redirectTo: 'dashboard' //redirecionamento.
+    }
+    
+];
