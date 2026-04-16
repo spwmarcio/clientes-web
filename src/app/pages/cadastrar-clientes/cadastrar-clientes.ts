191d0d77cf87cb730f869b3a418ee399d1a20f15
Marcio Fonseca de Souza <spwmarcio@gmail.com>
2026-04-17 00:32:38 +0100

    .

--- src/app/pages/cadastrar-clientes/cadastrar-clientes.ts
+++ src/app/pages/cadastrar-clientes/cadastrar-clientes.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
 @Component({
   selector: 'app-cadastrar-clientes',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   templateUrl: './cadastrar-clientes.html',
   styleUrl: './cadastrar-clientes.css',
 })
-export class CadastrarClientes {}
+export class CadastrarClientes {
+
+  // Instanciar a classe HTTPCLIENT
+  http = inject(HttpClient);
+
+// Função executada ao abrir o componente
+  ngOnInit() {
+    //console.log('Componente CadastrarClientes carregado');
+    // Fazendo uma chamada / requisição para o endpoint da API de clientes
+    // para consultar as opções do enum de níveis de cliente
+      this.http.get('http://localhost:5166/api/v1/Enum/niveis-cliente')
+      .subscribe((data) => {
+
+        console.log('Opções do enum de níveis de cliente:', data);
+      });
+  }
+
+}
