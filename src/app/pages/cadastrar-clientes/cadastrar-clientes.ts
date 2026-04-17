1a5df49203fea0888bb3c0eb1ad89ccfb2430639
Marcio Fonseca de Souza <spwmarcio@gmail.com>
2026-04-17 01:56:46 +0100

    .

--- src/app/pages/cadastrar-clientes/cadastrar-clientes.ts
+++ src/app/pages/cadastrar-clientes/cadastrar-clientes.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-clientes',
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastrar-clientes.html',
   styleUrl: './cadastrar-clientes.css',
@@ -17,6 +20,15 @@ export class CadastrarClientes {
 
   // Instanciar a classe HTTPCLIENT
   http = inject(HttpClient);
+
+  // formulario para capturar os campos do cadastro do cliente
+  formcadastro = new FormGroup({
+    nome : new FormControl('',[Validators.required,Validators.minLength(6),Validators.maxLength(150)]),
+    cpf : new FormControl('',[Validators.required,Validators.minLength(11),Validators.maxLength(11)]),
+    email : new FormControl('',[Validators.required,Validators.email]),
+    telefone : new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(11)]),
+    nivel : new FormControl('',[Validators.required]),
+  })
 
 // Função executada ao abrir o componente
   ngOnInit() {
@@ -31,4 +43,20 @@ export class CadastrarClientes {
       });
   }
 
+  //Função para capturar o evento de SUBMIT do formulário
+  cadastrarCliente() {
+    //Fazendo uma requisição POST para a API (Cadastro do cliente)
+    this.http.post('http://localhost:5166/api/v1/clientes/cadastrar-cliente', this.formcadastro.value)
+      .subscribe({
+        next : (data) => { //capturando o retorno de sucesso da API
+          console.log(data);
+        },
+        error : (e) => { //capturando o retorno de erro da API
+          console.log(e.error);
+        }
+      })
+  }
+
 }
+
+
